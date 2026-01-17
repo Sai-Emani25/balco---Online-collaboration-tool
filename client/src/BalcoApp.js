c6e8dcf523231ad99df90091a3edd95d99c0e3ec
Sai Emani <mrshanmukha06@gmail.com>
2026-01-17 21:53:49 +0530

    Fix blank page: Switch to HashRouter and update build artifacts

--- client/src/BalcoApp.js
+++ client/src/BalcoApp.js
@@ -1,19 +1,17 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import BalcoHome from './pages/BalcoHome';
 import BalcoRoom from './pages/BalcoRoom';
 import './BalcoApp.css';
 
 function BalcoApp() {
-  // Use PUBLIC_URL so GitHub Pages serves routes under the repo path
-  const basename = process.env.PUBLIC_URL || '/balco---Online-collaboration-tool';
-
   return (
-    <Router basename={basename}>
+    <Router>
       <div className="App">
         <Routes>
           <Route path="/" element={<BalcoHome />} />
           <Route path="/room/:roomId" element={<BalcoRoom />} />
+          <Route path="*" element={<BalcoHome />} />
         </Routes>
       </div>
     </Router>
